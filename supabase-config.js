ae1249c33c94a4e931fb99a37d0904235166e0e8
Mavinos <147985005+Mavinos@users.noreply.github.com>
2026-09-12 23:53:15 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Colle ici les valeurs de ton projet Supabase (Settings > API).
-window.AUTOLEAD_SUPABASE_URL = "sb_publishable_uJBQEjTN0LKw4-nyW3_Nag_sVlLIvFF";
-window.AUTOLEAD_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzZGZ5Y3ppdWlxYmNmbmt2d3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTI3MDIsImV4cCI6MjEwNDcyODcwMn0.Ccgp6M2UHNieCHaPQqZhPTiXoHHa4Y0z3ag2HqwjmZk";
+window.AUTOLEAD_SUPABASE_URL = "https://ssdfycziuiqbcfnkvwsm.supabase.co";
+window.AUTOLEAD_SUPABASE_ANON_KEY = "sb_publishable_uJBQEjTN0LKw4-nyW3_Nag_sVlLIvFF";
